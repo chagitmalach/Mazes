b3c2fab891a03b40cb49ff2b9b33474d23ee50ae
Hagit Malach <hmalach@salesforce.com>
2020-06-10 20:50:21 +0300

    commit first

---
+++ src/controllers/mazes.controller.ts
@@ -0,0 +1,12 @@
+export class MazesController {
+
+    solveMaze(mazeMatrix: Array<Array<string>>): Array<string> {
+
+        if (!mazeMatrix || !mazeMatrix.length) {
+            return undefined;
+        }
+        
+        const result = new Array<string>();
+        return result;
+    }
+}
