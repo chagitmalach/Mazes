731454f99070b9606f78bbeefd52347a99ca763e
Hagit Malach <hmalach@salesforce.com>
2020-06-14 13:50:55 +0300

    Add test for row and entry

--- src/controllers/mazes.controller.spec.ts
+++ src/controllers/mazes.controller.spec.ts
@@ -9,32 +9,69 @@ describe('Mazes controller tests', () => {
 
     describe('SolveMaze tests', () => {
 
-        it('mazeMatrix is null return undefined', () => {
+        it('mazeMatrix is null throw error maze is invalid', () => {
 
             var mazeMatrix = null;
 
-            var result = mazeController.solveMaze(mazeMatrix);
+            try {
+                mazeController.solveMaze(mazeMatrix);
+            }
+            catch (err) {
+                expect(err.message).eq('maze is invalid');
+            }
 
-            expect(result).to.undefined;
         });
 
-        it('mazeMatrix is undefined return undefined', () => {
+        it('mazeMatrix is undefined throw error maze is invalid', () => {
 
             var mazeMatrix = undefined;
 
-            var result = mazeController.solveMaze(mazeMatrix);
-
-            expect(result).to.undefined;
+            try {
+                mazeController.solveMaze(mazeMatrix);
+            }
+            catch (err) {
+                expect(err.message).eq('maze is invalid');
+            }
         });
 
-        it('mazeMatrix length in zero return undefined', () => {
+        it('mazeMatrix length in zero throw error maze is invalid', () => {
 
             var mazeMatrix = new Array<Array<string>>();
 
-            var result = mazeController.solveMaze(mazeMatrix);
-
-            expect(result).to.undefined;
+            try {
+                mazeController.solveMaze(mazeMatrix);
+            }
+            catch (err) {
+                expect(err.message).eq('maze is invalid');
+            }
         });
 
+        it('there no rows in maze throw error no rows in maze', () => {
+
+            var mazeMatrix = new Array<Array<string>>();
+            mazeMatrix[0] = new Array<string>();
+           
+            try {
+                mazeController.solveMaze(mazeMatrix);
+            }
+            catch (err) {
+                expect(err.message).eq('there are no rows in maze');
+            }
+        });
+
+        it('there no entry in the first row throw error no entry in maze', () => {
+
+            var mazeMatrix = new Array<Array<string>>();
+            mazeMatrix[0] = new Array<string>();
+            mazeMatrix[0][0] = "1";
+            mazeMatrix[0][1] = "12";
+
+            try {
+                mazeController.solveMaze(mazeMatrix);
+            }
+            catch (err) {
+                expect(err.message).eq('there is no entry in maze');
+            }
+        });
     });
 });
--- src/controllers/mazes.controller.ts
+++ src/controllers/mazes.controller.ts
@@ -3,13 +3,13 @@ export class MazesController {
     solveMaze(mazeMatrix: Array<Array<string>>): Array<string> {
 
         if (!mazeMatrix || !mazeMatrix.length) {
-            return undefined;
+            throw new Error("maze is invalid");
         }
 
         const entryColumnIndex = this.findMazeEntry(mazeMatrix[0]);
 
         if (entryColumnIndex === -1) {
-            throw new Error();
+            throw new Error("there is no entry in maze");
         }
 
         const result = new Array<string>();
@@ -19,10 +19,10 @@ export class MazesController {
     private findMazeEntry(firstRow: Array<string>): number {
 
         if (!firstRow.length) {
-            throw new Error();
+            throw new Error("there are no rows in maze");
         }
 
-        const indexEntry = firstRow.findIndex(val => val == undefined);
+        const indexEntry = firstRow.indexOf(undefined);
         return indexEntry;
     }
 }
